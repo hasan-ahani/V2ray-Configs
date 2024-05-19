51f87ec9eed8bfcd7b46b0bdd96182116fb5f9aa
Hassan Ahani <67734902+hasan-ahani@users.noreply.github.com>
2024-05-19 13:58:17 +0330

    Update app.py

--- Files/app.py
+++ Files/app.py
@@ -138,7 +138,7 @@ def main():
         lines = f.readlines()
 
     num_lines = len(lines)
-    max_lines_per_file = 50
+    max_lines_per_file = 80
     num_files = (num_lines + max_lines_per_file - 1) // max_lines_per_file
 
     for i in range(num_files):
